9f7c28964240e1e329c41837321aacd614b3fc35
TristanVarewijck <74105718+TristanVarewijck@users.noreply.github.com>
2020-11-27 09:00:42 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
 // JavaScript Document
+var menuButton = document.querySelector(".nav-menu");
+
+menuButton.addEventListener('click', menuSlide); 
+
+function menuSlide(){
+    let hetMenu = document.querySelector(".toggle"); 
+    hetMenu.classList.toggle(".toonMenu");
+}
